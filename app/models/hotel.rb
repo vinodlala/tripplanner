81a62d9a1805a7eaf5287ffcaa7528a52bd05b9c
Jeffrey Willson <peacetoawarship@gmail.com>
2013-10-28 17:19:52 -0400

    added associations and Hotel.get_all

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,21 @@
 class Hotel < ActiveRecord::Base
+  belongs_to :place
+
+  def self.get_all
+    self.all.map do |i|
+      pl = i.place
+    {
+    :name => i.name,
+    :address => pl.address,
+    :city => pl.city,
+    :state => pl.state,
+    :phone => pl.phone,
+    :lat => pl.lat,
+    :lon => pl.lon,
+    :num_stars => i.num_stars,
+    :amenities => i.amenities
+    }
+    end
+  end
 
 end
--- app/models/thing_to_do.rb
+++ app/models/thing_to_do.rb
@@ -1,2 +1,3 @@
 class ThingToDo < ActiveRecord::Base
+  belongs_to :place
 end
