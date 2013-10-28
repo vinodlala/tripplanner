151697f60784c90b1b291762659c86b8fb6136e2
Vinod Lala <vinod_lala@usa.net>
2013-10-28 17:34:04 -0400

    place and restaurant models

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,19 @@ class Place < ActiveRecord::Base
   has_many :hotels
   has_many :thing_to_dos
   has_many :restaurants
+
+  def self.get_all
+    self.all.map do |i|
+      # pl = i.place
+      {
+        :name => i.name,
+        :address => i.address,
+        :city => i.city,
+        :state => i.state,
+        :phone => i.phone,
+        :lat => i.lat,
+        :lon => i.lon
+      }
+    end
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,22 @@
 class Restaurant < ActiveRecord::Base
+  belongs_to :place
+
+  def self.get_all
+    # @restaurants = Restaurant.all
+    self.all.map do |i|
+      pl = i.place
+      {
+        :name => pl.name,
+        :address => pl.address,
+        :city => pl.city,
+        :state => pl.state,
+        :phone => pl.phone,
+        :lat => pl.lat,
+        :lon => pl.lon,
+        :cuisine => i.cuisine,
+        :price => i.price
+      }
+    end
+  end
+
 end
