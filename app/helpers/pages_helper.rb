b6bcaf716ddf572d7fb5b9e9fd35da841dc624a3
Vinod Lala <vinod_lala@usa.net>
2013-10-28 17:53:25 -0400

    pages helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
 end
+
+def get_hotels
+  Hotel.get_all.to_json.html_safe
+end
+
+def get_restaurants
+  Restaurant.get_all.to_json.html_safe
+end
+
+def get_thingstodo
+  ThingToDo.get_all.to_json.html_safe
+end
