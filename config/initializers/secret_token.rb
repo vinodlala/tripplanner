9c1d40d1482ce06f056a35c92c639a14a11e4e5c
Vinod Lala <vinod_lala@usa.net>
2013-10-28 16:27:57 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripplanner::Application.config.secret_key_base = '6367a9c1594d9f6a4fa07c4e8f063e4820ad81d8e9a60e31ea97007dfe97344e36db0d492f0c7ca368de3e5be926122b9d0f4e74fda84a72c2095fade698aa88'
