6d0c42039a6bac01dbd4bb166f0a15f477107b47
Jeffrey Willson <peacetoawarship@gmail.com>
2013-10-30 15:22:26 -0400

    maps API for restaurants, and footer added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,50 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var current_marker; var map;
+
+var initialize_gmaps = function(lat, lng, name) {
+  var mapOptions, map_canvas_obj, myLatlng;
+  myLatlng = new google.maps.LatLng(lat, lng);
+  mapOptions = {
+    center: myLatlng,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map_canvas_obj = document.getElementById("map-canvas");
+  map = new google.maps.Map(map_canvas_obj, mapOptions);
+  current_marker = new google.maps.Marker({
+    position: myLatlng,
+    title: name
+  });
+  return current_marker.setMap(map);
+};
+
+// var add_marker = function(lat,lon,name) {
+//   if(typeof name == "undefined") {
+//     name = "";
+//   }
+//   var myLatlng = new google.maps.LatLng(lat, lon);
+//   var next_marker = new google.maps.Marker({
+//     position: myLatlng,
+//     title: name
+//   });
+//   next_marker.setMap(map);
+//   current_marker = next_marker;
+// };
+
+// var remove_marker = function() {
+//   current_marker.setMap(null);
+// };
+
+var move_marker = function (myLatlng,name) {
+  console.log('move marker called');
+  // var myLatlng = new google.maps.LatLng(lat, lon);
+  current_marker.setPosition(myLatlng);
+  current_marker.setTitle(name);
+};
+
+$(document).ready(function() {
+  return initialize_gmaps(40.705786, -74.007672, "Fullstack Academy");
+});
